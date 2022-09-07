a9414e350019d072d02837f6f88c262c535673e9
Leandro Soares <leoarcabold@gmail.com>
2022-09-06 22:01:10 -0300

    configuração da tabela

--- src/main/java/erp/model/Empresa.java
+++ src/main/java/erp/model/Empresa.java
@@ -35,24 +35,26 @@ public class Empresa implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column (name = "nome_fantasia")
+	@Column (name = "nome_fantasia", nullable = false, length = 80)
 	private String nomeFantasia;
 	
-	@Column(name = "razao_social")
+	@Column(name = "razao_social", nullable = false, length = 120)
 	private String razaoSocial;
 	
+	@Column(nullable = false, length = 18)
 	private String cnpj;
 	
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	@Column(name = "data_fundacao")
 	private Date dateFundacao;
 	
 	@ManyToOne
-	@JoinColumn(name = "ramo_atividade_id")
+	@JoinColumn(name = "ramo_atividade_id", nullable = false)
 	@Column(name = "ramo_atividade")
 	private String ramoAtividade;
 	
 	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 30)
 	private TipoEmpresa tipo;
 	
 		
