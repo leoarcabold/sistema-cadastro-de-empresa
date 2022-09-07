f112b527081b9b8073fc5f6972197e5ff35b4349
Leandro Soares <leoarcabold@gmail.com>
2022-09-06 22:01:44 -0300

    definição da tabela

--- src/main/java/erp/model/RamoAtividade.java
+++ src/main/java/erp/model/RamoAtividade.java
@@ -26,6 +26,7 @@ public class RamoAtividade implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(nullable = false, length = 80)
 	private String descricao;
 	
 
