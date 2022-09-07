e6deeeff76d486ed0391201eb6667c8a03f1998e
Leandro Soares <leoarcabold@gmail.com>
2022-09-07 00:35:56 -0300

    classes da aplicação

---
+++ src/main/java/erp/repository/SchemaGeneration.java
@@ -0,0 +1,26 @@
+package erp.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import erp.model.Empresa;
+
+public class SchemaGeneration {
+	
+	public static void main (String[] args) { 
+		
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("SistemaPU");		
+		EntityManager em = emf.createEntityManager();
+		
+		List<Empresa> lista =  em.createQuery("from Empresa", Empresa.class).getResultList();
+		
+		System.out.println(lista);
+		
+		em.close();
+		emf.close();
+		
+	}
+}
